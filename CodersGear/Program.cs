82b6276d054346bf2d654d0832e540331813f721
Anthony <anthony@flutterhive.dev>
2026-03-02 20:39:15 +0100

    Add PostgreSQL support for Railway deployment

--- CodersGear/Program.cs
+++ CodersGear/Program.cs
@@ -17,8 +17,24 @@ builder.Services.AddControllersWithViews();
 
 // Add API controllers for webhooks
 builder.Services.AddControllers();
-builder.Services.AddDbContext<ApplicationDbContext>(options=>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Configure database - use PostgreSQL if DATABASE_URL is available (Railway), otherwise SQL Server
+var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+if (!string.IsNullOrEmpty(databaseUrl))
+{
+    // Railway provides DATABASE_URL in format: postgres://user:pass@host:port/database
+    // Convert to Npgsql format: Host=host;Port=port;Database=database;Username=user;Password=pass
+    var uri = new Uri(databaseUrl);
+    var userInfo = uri.UserInfo.Split(':');
+    var connectionString = $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
+
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseNpgsql(connectionString));
+}
+else
+{
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+}
 
 builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("Stripe"));
 builder.Services.Configure<PrintifySettings>(builder.Configuration.GetSection("Printify"));
