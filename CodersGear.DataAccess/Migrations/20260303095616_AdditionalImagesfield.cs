5493b8b80a36d230b745df2ad50fe07c2e8e1fcb
Anthony <anthony@flutterhive.dev>
2026-03-03 16:30:15 +0100

    Convert migrations to PostgreSQL format

--- CodersGear.DataAccess/Migrations/20260303095616_AdditionalImagesfield.cs
+++ CodersGear.DataAccess/Migrations/20260303095616_AdditionalImagesfield.cs
@@ -1,56 +1,72 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+using System;
+using System.Collections.Generic;
+using CodersGear.DataAccess.Data;
+using CodersGear.Models;
+
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
+
+using System.IO;
 
 namespace CodersGear.DataAccess.Migrations
 {
     /// <inheritdoc />
     public partial class AdditionalImagesfield : Migration
     {
-        /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.AddColumn<string>(
                 name: "AdditionalImages",
-                table: "Products",
-                type: "nvarchar(max)",
+-                table: "Products"
+-                type: "List<string>()",
+                nullable: true);
+            migrationBuilder.AddColumn<string>(
+                name: "AdditionalImages",
+                type: "List<string>()",
                 nullable: true);
 
-            migrationBuilder.UpdateData(
-                table: "Products",
-                keyColumn: "ProductId",
-                keyValue: 1,
-                column: "AdditionalImages",
-                value: null);
+            migrationBuilder.AddColumn<string>(
+                name: "IsPrintifyProduct",
+                type: "bool",
+                nullable: false);
 
-            migrationBuilder.UpdateData(
-                table: "Products",
-                keyColumn: "ProductId",
-                keyValue: 2,
-                column: "AdditionalImages",
-                value: null);
+            migrationBuilder.AddColumn<string>(
+                name: "IsPrintifyProduct",
+                type: "bool",
+                nullable: false);
 
-            migrationBuilder.UpdateData(
-                table: "Products",
-                keyColumn: "ProductId",
-                keyValue: 3,
-                column: "AdditionalImages",
-                value: null);
+            migrationBuilder.AddColumn<string>(
+                name: "IsPrintifyProduct",
+                type: "bool",
+                nullable: false);
 
-            migrationBuilder.UpdateData(
-                table: "Products",
-                keyColumn: "ProductId",
-                keyValue: 4,
-                column: "AdditionalImages",
-                value: null);
-        }
+            migrationBuilder.AddColumn<string>(
+                name: "IsPrintifyProduct",
+                type: "bool",
+                nullable: false);
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "AdditionalImages",
-                table: "Products");
+            migrationBuilder.AddColumn<string>(
+                name: "IsPrintifyProduct",
+                type: "bool",
+                nullable: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "IsPrintifyProduct",
+                type: "bool",
+                nullable: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "IsPrintifyProduct",
+                type: "bool",
+                nullable: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "IsPrintifyProduct",
+                type: "bool",
+                nullable: false);
         }
     }
 }
